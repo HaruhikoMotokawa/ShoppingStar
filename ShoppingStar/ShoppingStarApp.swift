60bb69d0141c4a019c0a2ad724e7bee6a1d16a11
本川　晴彦 <h.motokawa@salop.co.jp>
2024-01-07 00:41:48 +0900

    Initial Commit

---
+++ ShoppingStar/ShoppingStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingStarApp.swift
+//  ShoppingStar
+//
+//  Created by 本川晴彦 on 2024/01/07.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
