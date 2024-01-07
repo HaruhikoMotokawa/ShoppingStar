78d40124e48bd39e9a8a43cf546f0d352594c874
本川　晴彦 <h.motokawa@salop.co.jp>
2024-01-07 21:26:07 +0900

    主要なタブフォルダーを作成

---
+++ ShoppingStar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  ShoppingStar
+//
+//  Created by 本川晴彦 on 2024/01/07.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
