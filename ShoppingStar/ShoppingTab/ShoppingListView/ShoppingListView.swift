7efcc2a17b46f6b4918edda0678835bb8f58f341
本川　晴彦 <h.motokawa@salop.co.jp>
2024-01-08 18:23:20 +0900

    買い物リスト画面と買い物マップ画面を暫定で作成

---
+++ ShoppingStar/ShoppingTab/ShoppingListView/ShoppingListView.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingListView.swift
+//  ShoppingStar
+//
+//  Created by 本川晴彦 on 2024/01/08.
+//
+
+import SwiftUI
+
+struct ShoppingListView: View {
+    var body: some View {
+        Text("ShoppingListView")
+        .padding(.all, 100)
+        .background(.red)
+    }
+}
+
+#Preview {
+    ShoppingListView()
+}
