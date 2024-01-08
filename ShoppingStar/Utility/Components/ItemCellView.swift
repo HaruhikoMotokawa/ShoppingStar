c79dcfae937bdb8902e71d6151cc2a871c0e6b0b
本川　晴彦 <h.motokawa@salop.co.jp>
2024-01-08 22:34:37 +0900

    MainTabView.swiftの移動とItemCelViewの作成

---
+++ ShoppingStar/Utility/Components/ItemCellView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCellView.swift
+//  ShoppingStar
+//
+//  Created by 本川晴彦 on 2024/01/08.
+//
+
+import SwiftUI
+
+struct ItemCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemCellView()
+}
